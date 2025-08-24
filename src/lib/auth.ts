c1e656bfeb4f4b73ede2ee2a36b786fc70a54556
Lijan Haque <lijanhaque1122@gmail.com>
2025-08-24 03:42:55 +0000

    complete 3bf07d536922: Pull complete 5e67d63628c9: Pull complete 165dc54

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,7 +1,14 @@
 import { betterAuth } from "better-auth";
+import { sqliteAdapter } from "better-auth/adapters/sqlite";
+import Database from "better-sqlite3";
+
+const db = new Database(":memory:");
 
 export const auth = betterAuth({
     emailAndPassword: {
         enabled: true,
     },
+    database: sqliteAdapter({
+        db,
+    }),
 });
